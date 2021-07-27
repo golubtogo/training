28b7144c7b574a6ae0db87207f237dbe05c253ee
Nata <golub.togo@gmail.com>
2021-07-27 17:49:12 +0300

    add methods, parameters, groups

--- test_add_new_user.py
+++ test_add_new_user.py
@@ -5,6 +5,11 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import GroupName
+from group import GroupEmail
+from group import GroupBdate
+from group import GroupAdate
+
 
 class TestAddNewUser(unittest.TestCase):
     def setUp(self):
@@ -17,162 +22,165 @@ class TestAddNewUser(unittest.TestCase):
     def test_add_new_user(self):
         driver = self.driver
         self.open_home_page(driver)
-        self.login(driver)
+        self.login(driver, username="admin", password="secret")
         self.add_new_user(driver)
-        self.add_name(driver)
-        self.add_photo(driver)
-        self.add_title(driver)
-        self.add_company(driver)
-        self.add_address(driver)
-        self.add_home(driver)
-        self.add_mobile(driver)
-        self.add_work(driver)
-        self.add_fax(driver)
-        self.add_email(driver)
-        self.add_homepage(driver)
-        self.add_bdate(driver)
-        self.add_adate(driver)
+        self.add_name(driver, GroupName(firstname="first name", middlename="middle name", lastname="last name", nickname="nickname"))
+        self.add_photo(driver, photo="C:\\Users\\Nata\\PycharmProjects\\test_image.png")
+        self.add_title(driver, title="title")
+        self.add_company(driver, company="company")
+        self.add_address(driver, address_phone="address\n+380111111111")
+        self.add_home(driver, home="home")
+        self.add_mobile(driver, mobile="mobile")
+        self.add_work(driver, work="work")
+        self.add_fax(driver, fax="fax")
+        self.add_email(driver, GroupEmail(email="e-mail", email_2="e-mail2", email_3="e-mail3"))
+        self.add_homepage(driver, homepage="homepage")
+        self.add_bdate(driver, GroupBdate(b_year="2000", b_month="January", b_day="1"))
+        self.add_adate(driver, GroupAdate(a_year="2015", a_month="February", a_day="1"))
         self.choose_group(driver)
-        self.add_address2(driver)
-        self.add_phone2(driver)
-        self.add_notes(driver)
+        self.add_secondary_address(driver, secondary_address="secondary address")
+        self.add_secondary_phone(driver, secondary_phone="secondary home")
+        self.add_notes(driver, notes="hello")
         self.submit(driver)
+
 
     def submit(self, driver):
         driver.find_element_by_xpath("//input[21]").click()
 
-    def add_notes(self, driver):
+    def add_notes(self, driver, notes):
         driver.find_element_by_name("notes").click()
         driver.find_element_by_name("notes").clear()
-        driver.find_element_by_name("notes").send_keys("hello")
+        driver.find_element_by_name("notes").send_keys(notes)
 
-    def add_phone2(self, driver):
+    def add_secondary_phone(self, driver, secondary_phone):
         driver.find_element_by_name("phone2").click()
         driver.find_element_by_name("phone2").clear()
-        driver.find_element_by_name("phone2").send_keys("secondary home")
+        driver.find_element_by_name("phone2").send_keys(secondary_phone)
 
-    def add_address2(self, driver):
+    def add_secondary_address(self, driver, secondary_address):
         driver.find_element_by_name("address2").click()
         driver.find_element_by_name("address2").clear()
-        driver.find_element_by_name("address2").send_keys("secondary address")
+        driver.find_element_by_name("address2").send_keys(secondary_address)
 
     def choose_group(self, driver):
         driver.find_element_by_name("new_group").click()
         driver.find_element_by_xpath("//option[@value='[none]']").click()
 
-    def add_adate(self, driver):
+    def add_adate(self, driver, group):
         # add ayear
         driver.find_element_by_name("ayear").click()
         driver.find_element_by_name("ayear").clear()
-        driver.find_element_by_name("ayear").send_keys("2015")
+        driver.find_element_by_name("ayear").send_keys(group.a_year)
         # add amonth
         driver.find_element_by_name("amonth").click()
-        Select(driver.find_element_by_name("amonth")).select_by_visible_text("February")
+        Select(driver.find_element_by_name("amonth")).select_by_visible_text(group.a_month)
         driver.find_element_by_xpath("//select[4]/option[3]").click()
         # add aday
         driver.find_element_by_name("aday").click()
-        Select(driver.find_element_by_name("aday")).select_by_visible_text("1")
+        Select(driver.find_element_by_name("aday")).select_by_visible_text(group.a_day)
         driver.find_element_by_xpath("//select[3]/option[3]").click()
 
 
-    def add_bdate(self, driver):
+    def add_bdate(self, driver, group):
         # add byear
         driver.find_element_by_name("byear").click()
         driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys("2000")
+        driver.find_element_by_name("byear").send_keys(group.b_year)
         # add bmonth
         driver.find_element_by_name("bmonth").click()
-        Select(driver.find_element_by_name("bmonth")).select_by_visible_text("January")
+        Select(driver.find_element_by_name("bmonth")).select_by_visible_text(group.b_month)
         driver.find_element_by_xpath("//select[2]/option[2]").click()
         # add dday
         driver.find_element_by_name("bday").click()
-        Select(driver.find_element_by_name("bday")).select_by_visible_text("1")
+        Select(driver.find_element_by_name("bday")).select_by_visible_text(group.b_day)
         driver.find_element_by_xpath("//option[3]").click()
 
-    def add_homepage(self, driver):
+    def add_homepage(self, driver, homepage):
         driver.find_element_by_name("homepage").click()
         driver.find_element_by_name("homepage").clear()
-        driver.find_element_by_name("homepage").send_keys("homepage")
+        driver.find_element_by_name("homepage").send_keys(homepage)
 
-    def add_email(self, driver):
+    def add_email(self, driver, GroupEmail):
         # add email
         driver.find_element_by_name("email").click()
         driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("e-mail")
+        driver.find_element_by_name("email").send_keys(GroupEmail.email)
         # add email 2
         driver.find_element_by_name("email2").click()
         driver.find_element_by_name("email2").clear()
-        driver.find_element_by_name("email2").send_keys("e-mail2")
+        driver.find_element_by_name("email2").send_keys(GroupEmail.email_2)
         # add email 3
         driver.find_element_by_name("email3").click()
         driver.find_element_by_name("email3").clear()
-        driver.find_element_by_name("email3").send_keys("e-mail3")
+        driver.find_element_by_name("email3").send_keys(GroupEmail.email_3)
 
-    def add_fax(self, driver):
+    def add_fax(self, driver, fax):
         driver.find_element_by_name("fax").click()
         driver.find_element_by_name("fax").clear()
-        driver.find_element_by_name("fax").send_keys("fax")
+        driver.find_element_by_name("fax").send_keys(fax)
 
-    def add_work(self, driver):
+    def add_work(self, driver, work):
         driver.find_element_by_name("work").click()
         driver.find_element_by_name("work").clear()
-        driver.find_element_by_name("work").send_keys("work")
+        driver.find_element_by_name("work").send_keys(work)
 
-    def add_mobile(self, driver):
+    def add_mobile(self, driver, mobile):
         driver.find_element_by_name("mobile").click()
         driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys("mobile")
+        driver.find_element_by_name("mobile").send_keys(mobile)
 
-    def add_home(self, driver):
+    def add_home(self, driver, home):
         driver.find_element_by_name("home").click()
         driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys("home")
+        driver.find_element_by_name("home").send_keys(home)
 
-    def add_address(self, driver):
+    def add_address(self, driver, address_phone):
         driver.find_element_by_name("address").click()
         driver.find_element_by_name("address").click()
         driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys("address\n+380111111111")
+        driver.find_element_by_name("address").send_keys(address_phone)
 
-    def add_company(self, driver):
+    def add_company(self, driver, company):
         driver.find_element_by_name("company").click()
         driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys("company")
+        driver.find_element_by_name("company").send_keys(company)
 
-    def add_title(self, driver):
+    def add_title(self, driver, title):
         driver.find_element_by_name("title").click()
         driver.find_element_by_name("title").clear()
-        driver.find_element_by_name("title").send_keys("title")
+        driver.find_element_by_name("title").send_keys(title)
 
-    def add_photo(self, driver):
-        driver.find_element_by_name("photo").send_keys("C:\\Users\\Nata\\PycharmProjects\\test_image.png")
+    def add_photo(self, driver, photo):
+        driver.find_element_by_name("photo").send_keys(photo)
 
-    def add_name(self, driver):
+    def add_name(self, driver, group):
         # add firstname
         driver.find_element_by_name("firstname").click()
         driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("first name")
+        driver.find_element_by_name("firstname").send_keys(group.firstname)
         # add middlename
         driver.find_element_by_name("middlename").click()
         driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys("middle name")
+        driver.find_element_by_name("middlename").send_keys(group.middlename)
         # add lastname
         driver.find_element_by_name("lastname").click()
         driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("last name")
+        driver.find_element_by_name("lastname").send_keys(group.lastname)
         # add nickname
         driver.find_element_by_name("nickname").click()
         driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys("nickname")
+        driver.find_element_by_name("nickname").send_keys(group.nickname)
 
     def add_new_user(self, driver):
         driver.find_element_by_link_text("add new").click()
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_home_page(self, driver):
