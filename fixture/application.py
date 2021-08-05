77c4aa296550e0d6949f930df6df752f8a741406
Nata <golub.togo@gmail.com>
2021-08-05 16:48:32 +0300

    made one class Application for all helpers

--- fixture/application.py
+++ fixture/application.py
@@ -11,21 +11,6 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.user = UserHelper(self)
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost:80/addressbook/edit.php")
-
-    def destroy(self):
-        self.wd.quit()
-
-
-class ApplicationGroup:
-
-    def __init__(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(60)
-        self.session = SessionHelper(self)
         self.group = GroupHelper(self)
 
     def open_home_page(self):
@@ -34,3 +19,6 @@ class ApplicationGroup:
 
     def destroy(self):
         self.wd.quit()
+
+
+
