d67b4bfa0a6373fe52c9b993ca1a6e407c10ef12
Nata <golub.togo@gmail.com>
2021-08-19 16:41:33 +0300

    fix updating modify_first_user

--- test/test_modify_user.py
+++ test/test_modify_user.py
@@ -4,8 +4,16 @@ from model.user import User
 
 
 def test_modify_first_user(app):
+    if app.user.count() == 0:
+        app.user.create_user(User())
     app.user.modify_first_user(User(
-              lastname="new lastname", firstname="new firstname",
+              lastname="new 1", firstname="new firstname",
               address="new address", home="new home", mobile="new mobile",
               work="new work", phone2="secondary phone",
               email="new_email", email2="new_email_2", email3="new_email_3"))
+
+
+# def test_modify_first_user_mobile(app):
+#    if app.user.count() == 0:
+#        app.user.create_user(User())
+#    app.user.modify_first_user(User(mobile="mobile modified"))
