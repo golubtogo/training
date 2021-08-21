81fb111dc3aa17c433b820be4a525e1fe96da746
Nata <golub.togo@gmail.com>
2021-08-21 20:02:12 +0300

    fix stopping tests after logout or closing browser

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.common.exceptions import InvalidSelectorException, NoSuchElementException
 
 from fixture.session import SessionHelper
 from fixture.user import UserHelper
@@ -11,16 +10,16 @@ class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
 
-        self.wd.implicitly_wait(1)
+        self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.user = UserHelper(self)
         self.group = GroupHelper(self)
 
     def is_valid(self):
         try:
-            if self.wd.find_element_by_xpath("//a[contains(text(),'Logout')]"):
-                return True
-        except NoSuchElementException:
+            self.wd.current_url
+            return True
+        except:
             return False
 
     def open_home_page(self):
