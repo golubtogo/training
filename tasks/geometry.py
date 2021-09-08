b6914181271f2d33118f0c18bbd11fc0fac86c55
Nataliya Golub <golub.togo@gmail.com>
2021-09-08 16:17:47 +0300

    Homework 11 (#3)
    * resolve conflict
    
    * remove driver log
    
    * homework 11
    
    * HW 11.2 cache
    
    * cache
    
    * update gitignore
    
    * add choose random

--- tasks/geometry.py
+++ tasks/geometry.py
@@ -6,3 +6,23 @@ a = Point(0, 0)
 b = Point(6, 7)
 
 print(a.distance(b))
+
+l1 = [Point(3, 1), Point(0, 0), Point(1, 2)]
+
+
+l2 = sorted(l1, key=lambda p: p.distance(Point(0, 0)))
+print(l1)
+print(l2)
+
+# l = [Point(i, i*i) for i in range(-5, 6)]
+l = list(map(lambda i: Point(i, i*i), range(-5, 6)))
+
+
+# for el in l:
+#     l2.append(Point(el.x, -el.y))
+
+# l2 = list(map(lambda p: Point(p.x, -p.y), l))
+l2 = list(filter(lambda p: p.x % 2 == 0, l))
+
+print(l)
+print(l2)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,27 +1,45 @@
 # -*- coding: utf-8 -*-
 
 from model.group import Group
+from random import randrange
 
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
-        app.group.create_group(Group(group_name=""))
-    app.group.modify_first_group(Group(group_name="modify name"))
+        app.group.create_group(Group(group_name="test modify"))
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+    group = Group(group_name="modify name 5555")
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == app.group.count()
+    old_groups[index] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_clean_group_name(app):
-    if app.group.count() == 0:
-        app.group.create_group(Group(group_name="test name"))
-    app.group.modify_first_group(Group(group_name=""))
-
-
-def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create_group(Group(group_header=""))
-    app.group.modify_first_group(Group(group_header="modify header"))
-
-
-def test_clean_group_header(app):
-    if app.group.count() == 0:
-        app.group.create_group(Group(group_header="modify header"))
-    app.group.modify_first_group(Group(group_header=""))
+# def test_clean_group_name(app):
+#     old_groups = app.group.get_group_list()
+#     if app.group.count() == 0:
+#         app.group.create_group(Group(group_name="test name"))
+#     app.group.modify_first_group(Group(group_name=""))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
+#
+#
+# def test_modify_group_header(app):
+#     old_groups = app.group.get_group_list()
+#     if app.group.count() == 0:
+#         app.group.create_group(Group(group_header=""))
+#     app.group.modify_first_group(Group(group_header="modify header"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
+#
+#
+# def test_clean_group_header(app):
+#     old_groups = app.group.get_group_list()
+#     if app.group.count() == 0:
+#         app.group.create_group(Group(group_header="modify header"))
+#     app.group.modify_first_group(Group(group_header=""))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
